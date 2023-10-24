765a4497a51384d5d621524fdeac48f52b8070e3
Darlyn30 <105079724+Darlyn30@users.noreply.github.com>
2023-10-24 15:46:20 -0700

    Add files via upload
    primer commit, hasta ahora esta hecho solo el visual, aun no tiene funciones,

---
+++ checkers/main.js
@@ -0,0 +1,96 @@
+// declare variables
+
+// tablero
+
+var moves = [crema1]
+
+// crema
+
+var crema1 = document.getElementById("crema1")
+var crema2 = document.getElementById("crema2")
+var crema3 = document.getElementById("crema3")
+var crema4 = document.getElementById("crema4")
+var crema5 = document.getElementById("crema5")
+var crema6 = document.getElementById("crema6")
+var crema7 = document.getElementById("crema7")
+var crema8 = document.getElementById("crema8")
+var crema9 = document.getElementById("crema9")
+var crema10 = document.getElementById("crema10")
+var crema11 = document.getElementById("crema11")
+var crema12 = document.getElementById("crema12")
+var crema13 = document.getElementById("crema13")
+var crema14 = document.getElementById("crema14")
+var crema15 = document.getElementById("crema15")
+var crema16 = document.getElementById("crema16")
+var crema17 = document.getElementById("crema17")
+var crema18 = document.getElementById("crema18")
+var crema19 = document.getElementById("crema19")
+var crema20 = document.getElementById("crema20")
+var crema21 = document.getElementById("crema21")
+var crema22 = document.getElementById("crema22")
+var crema23 = document.getElementById("crema23")
+var crema24 = document.getElementById("crema24")
+var crema25 = document.getElementById("crema25")
+var crema26 = document.getElementById("crema26")
+var crema27 = document.getElementById("crema27")
+var crema28 = document.getElementById("crema28")
+var crema29 = document.getElementById("crema29")
+var crema30 = document.getElementById("crema30")
+var crema31 = document.getElementById("crema31")
+var crema32 = document.getElementById("crema32")
+
+var botonBlanco = document.getElementById("botonBlanco")
+var botonNegro = document.getElementById("botonNegro")
+
+var movidasBlancas = document.getElementById("movesBlanco")
+var movidasNegras = document.getElementById("movesNegro")
+
+
+var movesBlanco = [0]
+var movesNegro = [0]
+
+const turnos = [1,2] // las blancas tendran turno 1 y las negras turno 2
+
+botonNegro.style.color = "gray"
+
+function moveBlanco(){
+    botonBlanco.style.color = "gray"
+    botonNegro.style.color = "black"
+
+
+
+}
+botonBlanco.addEventListener("click", moveBlanco)
+
+function moveNegro(){
+    botonBlanco.style.color = "black"
+    botonNegro.style.color = "gray"
+}
+
+botonNegro.addEventListener("click", moveNegro)
+/* FICHAS BLANCAS
+
+*/
+
+/* FICHAS NEGRAS
+
+ */
+
+
+// POSICIONES FICHAS NEGRAS
+
+// POSICIONES FICHAS NEGRAS
+
+// POSICIONES FICHAS BLANCAS
+
+// POSICIONES FICHAS BLANCAS
+
+// filas negras
+
+
+// filas blancas
+
+// excepcion de ficha
+
+var fichaReina
+
