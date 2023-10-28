e4f0db6d028b224f6f0641c8864fe1641b9be9d8
Darlyn30 <105079724+Darlyn30@users.noreply.github.com>
2023-10-28 16:07:57 -0400

    Add files via upload
    tratando de como manejar el DOM

---
+++ checkers-2/main.js
@@ -0,0 +1,40 @@
+const fichaBlancaInicial1 = document.getElementById("fichaBlancaRow1a1")
+const fichaBlancaInicial2 = document.getElementById("fichaBlancaRow1a2")
+const fichaBlancaInicial3 = document.getElementById("fichaBlancaRow1a3")
+const fichaBlancaInicial4 = document.getElementById("fichaBlancaRow1a4")
+const fichaBlancaInicial5 = document.getElementById("fichaBlancaRow2a1")
+const fichaBlancaInicial6 = document.getElementById("fichaBlancaRow2a2")
+const fichaBlancaInicial7 = document.getElementById("fichaBlancaRow2a3")
+const fichaBlancaInicial8 = document.getElementById("fichaBlancaRow2a4")
+const fichaBlancaInicial9 = document.getElementById("fichaBlancaRow3a1")
+const fichaBlancaInicial10 = document.getElementById("fichaBlancaRow3a2")
+const fichaBlancaInicial11 = document.getElementById("fichaBlancaRow3a3")
+const fichaBlancaInicial12 = document.getElementById("fichaBlancaRow3a4")
+
+const fichaNeegraInicial1 = document.getElementById("")
+const fichaNeegraInicial2 = document.getElementById("")
+const fichaNeegraInicial3 = document.getElementById("")
+const fichaNeegraInicial4 = document.getElementById("")
+const fichaNeegraInicial5 = document.getElementById("")
+const fichaNeegraInicial6 = document.getElementById("")
+const fichaNeegraInicial7 = document.getElementById("")
+const fichaNeegraInicial8 = document.getElementById("")
+const fichaNeegraInicial9 = document.getElementById("")
+const fichaNeegraInicial10 = document.getElementById("")
+const fichaNeegraInicial11 = document.getElementById("")
+const fichaNeegraInicial12 = document.getElementById("")
+
+// const posicionDisponibleInicioJuego 
+
+    const posicionDisponibleInicioJuego1  = document.getElementById("casillaNegraRow4a1")
+    const posicionDisponibleInicioJuego2  = document.getElementById("casillaNegraRow4a2")
+    const posicionDisponibleInicioJuego3  = document.getElementById("casillaNegraRow4a3")
+    const posicionDisponibleInicioJuego4  = document.getElementById("casillaNegraRow4a4")
+    const posicionDisponibleInicioJuego5  = document.getElementById("casillaNegraRow5a1")
+    const posicionDisponibleInicioJuego6  = document.getElementById("casillaNegraRow5a2")
+    const posicionDisponibleInicioJuego7  = document.getElementById("casillaNegraRow5a3")
+    const posicionDisponibleInicioJuego8  = document.getElementById("casillaNegraRow5a4")
+
+
+
+
